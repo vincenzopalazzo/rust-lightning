8ef9b4811aab64c758d3cf694e3d66162b90f950
Joost Jager <joost.jager@gmail.com>
2025-07-31 16:29:23 +0200

    Fix background processor log formatting
    Rustfmt is not always processing macros.

--- lightning-background-processor/src/lib.rs
+++ lightning-background-processor/src/lib.rs
@@ -1508,7 +1508,7 @@ impl BackgroundProcessor {
 							NETWORK_GRAPH_PERSISTENCE_KEY,
 							&network_graph.encode(),
 						) {
-							log_error!(logger, "Error: Failed to persist network graph, check your disk and permissions {}",e);
+							log_error!(logger, "Error: Failed to persist network graph, check your disk and permissions {}", e);
 						}
 						have_pruned = true;
 					}
@@ -1537,10 +1537,7 @@ impl BackgroundProcessor {
 							SCORER_PERSISTENCE_KEY,
 							&scorer.encode(),
 						) {
-							log_error!(logger,
-						"Error: Failed to persist scorer, check your disk and permissions {}",
-						e,
-					);
+							log_error!(logger, "Error: Failed to persist scorer, check your disk and permissions {}", e);
 						}
 					}
 					last_scorer_persist_call = Instant::now();
